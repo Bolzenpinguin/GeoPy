44833555ce03330cc25232a1346cd6a62db80b71
Bolzenpinguin <68389916+Bolzenpinguin@users.noreply.github.com>
2023-12-01 11:10:57 +0100

    - code clean up - corrected calculation of CalcGPSinEXIF - get frame rate from video - changed naming file of jpg to a number and read files in based on the number (sorted)

--- src/main.py
+++ src/main.py
@@ -1,32 +1,15 @@
 # ************************************ check if all modules are installed *********************************
 import shutil
-import platform
-import subprocess
 import os
 import sys
+import cv2
+import piexif
+from PIL import Image
+
+# ****************************************** preparation ******************************************
 
 # set to False if you want to keep all images without Metadata
-cleanUp = True
-
-try:
-    import cv2
-except ModuleNotFoundError:
-    print("Module cv2 is not installed, please install it first.")
-    exit(2)
-
-try:
-    import piexif
-except ModuleNotFoundError:
-    print("Module piexif is not installed, please install it first.")
-    exit(4)
-
-try:
-    from PIL import Image
-except ModuleNotFoundError:
-    print("Module PIL is not installed, please install it first.")
-    exit(5)
-
-# ****************************************** extract the frames ******************************************
+cleanUp = False
 
 # create the output directory if it doesn't already exist
 outputDir = 'output_frames'
@@ -38,53 +21,54 @@ videoPath = sys.argv[1]
 # check if video exits and if is readable
 if not os.path.exists(videoPath):
     print(f"Video file '{videoPath}' does not exist.")
-    exit(6)
+    exit()
 
 if not os.access(videoPath, os.R_OK):
     print(f"Cannot read the video file '{videoPath}'.")
-    exit(7)
-
-# open the video file
-videoCap = cv2.VideoCapture(videoPath)
-
-# initialize frame counter and interval
-frameCounter = 0
-interval = 30  # extract one frame every 30 frames ( = every second)
-
-# loop through the frames in the video
-while videoCap.isOpened():
-    # read the next frame
-    ret, frame = videoCap.read()
-
-    if ret:
-        # increment the frame counter
-        frameCounter += 1
-
-        # if the frame counter is divisible by the interval, extract the frame
-        if frameCounter % interval == 0:
-            # save the frame as an image file
-            frameFilename = os.path.join(outputDir, f'frame{frameCounter}.jpg')
-            cv2.imwrite(frameFilename, frame)
-
-    else:
-        # if reached the end of the video, break out of the loop
-        break
-
-# release the video file
-videoCap.release()
-
-# ****************************************** extract the nmea Strings ******************************************
+    exit()
 
 nmeaPath = sys.argv[2]
 
 # check if nmea exits and if is readable
 if not os.path.exists(nmeaPath):
     print(f"NMEA file '{nmeaPath}' does not exist.")
-    exit(8)
+    exit()
 
 if not os.access(nmeaPath, os.R_OK):
     print(f"Cannot read the NMEA file '{nmeaPath}' .")
-    exit(9)
+    exit()
+
+# open the video file
+videoCap = cv2.VideoCapture(videoPath)
+
+# get Frame rate
+frameRate = videoCap.get(cv2.CAP_PROP_FPS)
+
+# ****************************************** extract the frames ******************************************
+
+frameCounter = 0
+interval = int(frameRate)
+sortingNumber = 0
+
+# loop through the frames in the video
+while videoCap.isOpened():
+    ret, frame = videoCap.read()
+
+    if ret:
+        frameCounter += 1
+
+        # if the frame counter is divisible by the interval, extract the frame
+        if frameCounter % interval == 0:
+            frameFilename = os.path.join(outputDir, f'{sortingNumber:05d}.jpg')
+            cv2.imwrite(frameFilename, frame)
+            sortingNumber += 1
+
+    else:
+        break
+
+videoCap.release()
+
+# ****************************************** extract the nmea Strings ******************************************
 
 # Array for all the lines in the NMEA FIle -> only read the $GPGGA lines
 arrayNMEAString = []
@@ -96,7 +80,6 @@ with open(nmeaPath, 'r') as nmeaFile:
 
 # ****************************************** write the metadata ******************************************
 
-# create output Folder
 outputDirComp = 'outputJPGGPS'
 if not os.path.exists(outputDirComp):
     os.makedirs(outputDirComp)
@@ -104,63 +87,54 @@ if not os.path.exists(outputDirComp):
 # give the place in the array of the nmea string
 count = 0
 
-# calculate the GPS times
-def calcGPS(number):
-    degrees = int(number // 100)
-    minutes = int(number % 100)
-    centiseconds = int(((number % 100) * 100) % 100)
 
-    return ((degrees, 1), (minutes, 1), (centiseconds, 100))
+def CalcGPSinEXIF(decimalDegrees):
+    """
+    example: 6724.449
+    degrees = int(6724.449 / 100) = 67
+    minutes = int(6724.449 - (67 * 100)) = int(6724.449 - 6700) = int(24.449) = 24
+    decimalMinutes = 6724.449 - (67 * 100) - 24 = 24.449 - 24 = 0.449
+    seconds = int(0.449 * 60) = int(26.94) = 26
+
+    return (67, 1), (24, 1), (26, 1)
+    -> The 1 mean that the value is finished -> (6700, 100) would be converted into 67
+    """
+    degrees = int(decimalDegrees / 100)
+    minutes = int(decimalDegrees - (degrees * 100))
+    decimalMinutes = decimalDegrees - (degrees * 100) - minutes
+    seconds = int(decimalMinutes * 60)
+
+    return (degrees, 1), (minutes, 1), (seconds, 1)
 
 
-# loop through all JPG files in output_frames
-for fileName in os.listdir('output_frames'):
+# sorted the files from 0 to the highest numer and write the metadata
+for fileName in sorted(os.listdir('output_frames')):
     if fileName.endswith('.jpg'):
-        # load image and original metadat
         imgPath = os.path.join('output_frames', fileName)
         exifDict = piexif.load(imgPath)
 
-        # if there are fewer points then frames, last point is set as default
-        if count >= len(arrayNMEAString):
-            count = len(arrayNMEAString) - 1
+        gpsLatRef = arrayNMEAString[count].split(',')[3]
+        gpsLongRef = arrayNMEAString[count].split(',')[5]
 
-        # get the data
-        gpsLatRef = arrayNMEAString[count].split(',')[3].encode()
-        gpsLongRef = arrayNMEAString[count].split(',')[5].encode()
+        gpsLat = CalcGPSinEXIF(float(arrayNMEAString[count].split(',')[2]))
+        gpsLong = CalcGPSinEXIF(float(arrayNMEAString[count].split(',')[4]))
 
-        gpsLat = calcGPS(float(arrayNMEAString[count].split(',')[2]))
-        gpsLong = calcGPS(float(arrayNMEAString[count].split(',')[4]))
-
-        # modify GPS metadata
         gpsIfd = {
-            piexif.GPSIFD.GPSLongitude: gpsLong,
-            piexif.GPSIFD.GPSLongitudeRef: gpsLongRef,
             piexif.GPSIFD.GPSLatitude: gpsLat,
-            piexif.GPSIFD.GPSLatitudeRef: gpsLatRef
+            piexif.GPSIFD.GPSLatitudeRef: gpsLatRef.encode(),
+            piexif.GPSIFD.GPSLongitude: gpsLong,
+            piexif.GPSIFD.GPSLongitudeRef: gpsLongRef.encode()
         }
 
         exifDict["GPS"] = gpsIfd
 
-        # save modified images to output folder
         exifBytes = piexif.dump(exifDict)
         img = Image.open(imgPath)
         img.save(os.path.join(outputDirComp, fileName), exif=exifBytes)
-        count = count + 1
+        count += 1
 
 # ****************************************** clean up ******************************************
 
 # delete all images without GPS
 if cleanUp:
     shutil.rmtree(outputDir)
-
-# open folder with the images
-def openFile(path):
-    if platform.system() == "Windows":
-        os.startfile(path)
-    elif platform.system() == "Darwin":
-        subprocess.Popen(["open", path])
-    else:
-        subprocess.Popen(["xdg-open", path])
-
-openFile(outputDirComp)
-
