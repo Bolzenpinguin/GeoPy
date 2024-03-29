eabb108a51743292634c7577eae6288ec5c0610d
Kurt <schubert.geschaeftlich@gmail.com>
2024-03-29 10:42:51 +0100

    Refactor JS Code, added TODO's, still working on checkbox for keeping pictures without Metadata

--- src/main.js
+++ src/main.js
@@ -1,172 +1,103 @@
+// Get Elements by ID
+const elements = {
+  videoPath: document.getElementById('videoPath'),
+  nmeaPath: document.getElementById('nmeaPath'),
+  savePath: document.getElementById('savePath'),
+  bitmaskPath: document.getElementById('pathToBitmask'),
+  firstFrame: document.getElementById('firstFrame'),
+  firstNMEALine: document.getElementById('firstNMEALine'),
+  cleanup: document.getElementById('cleanup'),
+  startBtn: document.getElementById('btnStart'),
+  accordionBtn: document.querySelector('.accordion')
+};
 
-const pathToTheVideoFile = document.getElementById('videoPath');
-const btnPathToVideo = document.getElementById('btnVideoPath');
+// Get BTNs by ID
+const btns = {
+  videoPath: document.getElementById('btnVideoPath'),
+  nmeaPath: document.getElementById('btnNMEA'),
+  savePath: document.getElementById('btnSaveDic'),
+  bitmask: document.getElementById('getBitMaks')
+};
 
-const pathToTheNMEAFile = document.getElementById('nmeaPath');
-const btnPathToNMEA = document.getElementById('btnNMEA');
+// Check inputs for startBTN
+function areInputsValid() {
+  return elements.videoPath.value && elements.nmeaPath.value && elements.savePath.value;
+}
 
-const btnAccordion = document.querySelector('.accordion');
-
-const pathToTheSaveFolder = document.getElementById('savePath');
-const btnSavePlace = document.getElementById('btnSaveDic');
-
-const btnBitmask = document.getElementById('getBitMaks');
-const pathToBitmask = document.getElementById('pathToBitmask');
-
-const startFrameField = document.getElementById('firstFrame');
-const startNMEALineField = document.getElementById('firstNMEALine');
-
-const startBTN = document.getElementById('btnStart');
-
-
-// Start the Python Script
-startBTN.addEventListener('click', async () => {
-
-  // Int Defaults for the start lines
-  let startFrame= parseInt(startFrameField.value, 10);
-  let startNMEALine= parseInt(startNMEALineField.value, 10);
-
-  // Bitmask
-  let bitmaskProvided = true;
-  if (!isNaN(Number(pathToBitmask.value))) {
-    bitmaskProvided = false;
-  }
-
-  // cleanup
-  let cleanUp = document.getElementById('cleanup').checked;
-
-  console.log(cleanUp);
-
-  // set to default values if not declared
-  if (isNaN(startFrame) )
-    startFrame = 0;
-
-  if (isNaN(startNMEALine) )
-    startNMEALine = 0;
-
-  // create parameter list
-  const parameter = [
-      pathToTheVideoFile.value,
-      pathToTheNMEAFile.value,
-      pathToBitmask.value,
-      startFrame,
-      startNMEALine,
-      pathToTheSaveFolder.value,
-      bitmaskProvided,
-      cleanUp
-  ]
-
-  // prepare Array for transfer to backend -> convert everything to a string
-  const paramsObject = parameter.map(param => String(param)).reduce((obj, param, index) => {
-    obj[`param${index + 1}`] = param;
-    return obj;
-  }, {});
-
-
-  // run python script
-  window.__TAURI__.invoke('run_backend', paramsObject);
-
-});
-
-// Open Bitmask File
-btnBitmask.addEventListener('click', async () => {
-  try {
-    const selected = await window.__TAURI__.dialog.open({
-      filters: [
-        {name: 'Bitmask file', extensions: ['txt']}
-      ],
-      directory: false,
-      multiple: false,
-    });
-    if (selected) {
-      pathToBitmask.value = selected;
-      document.getElementById('getBitMaks').innerHTML = `
-        File Selected
-        <img src="assets/icons/check-svgrepo-com.svg" alt="File selected" width="24" height="24">
-      `;
+// Disable Start BTN on Load
+// TODO: BTN noch ausgrauen und erst activ machen, wenn Daten da sind
+document.addEventListener('DOMContentLoaded', () => {
+  elements.startBtn.disabled = !areInputsValid();
+  Object.values(elements).forEach(inputElement => {
+    if (['INPUT', 'SELECT'].includes(inputElement.tagName)) {
+      inputElement.addEventListener('input', () => {
+        elements.startBtn.disabled = !areInputsValid();
+      });
     }
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-// Open Saving Directory
-btnSavePlace.addEventListener('click', async () => {
-  try {
-    const selected = await window.__TAURI__.dialog.open({
-      directory: true,
-      multiple: false,
-    });
-    if (selected) {
-      pathToTheSaveFolder.value = selected;
-      document.getElementById('btnSaveDic').innerHTML = `
-        Directory Selected
-        <img src="assets/icons/check-svgrepo-com.svg" alt="File selected" width="24" height="24">
-      `;
-    }
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-// Open Video Explorer
-btnPathToVideo.addEventListener('click', async () => {
-  try {
-    const selected = await window.__TAURI__.dialog.open({
-      filters: [
-          {name: 'Video Files', extensions: ['mp4']}
-      ],
-      multiple: false,
-      directory: false,
-    });
-
-    if (selected) {
-      pathToTheVideoFile.value = selected;
-      document.getElementById('btnVideoPath').innerHTML = `
-        File Selected
-        <img src="assets/icons/check-svgrepo-com.svg" alt="File selected" width="24" height="24">
-      `;
-    }
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-// Open NMEA Explorer
-btnPathToNMEA.addEventListener('click', async () => {
-  try {
-    const selected = await window.__TAURI__.dialog.open({
-      filters: [
-        {name: 'NMEA Files', extensions: ['nmea']}
-      ],
-      multiple: false,
-      directory: false,
-    });
-
-    if (selected) {
-      pathToTheNMEAFile.value = selected;
-      document.getElementById('btnNMEA').innerHTML = `
-        File Selected
-        <img src="assets/icons/check-svgrepo-com.svg" alt="File selected" width="24" height="24">
-      `;
-    }
-  } catch (err) {
-    console.log(err);
-  }
+  });
 });
 
 // Open Accordion
-document.addEventListener('DOMContentLoaded', () => {
+elements.accordionBtn.addEventListener('click', function() {
+  this.classList.toggle('active');
+  const panel = this.nextElementSibling;
+  panel.style.maxHeight = this.classList.contains('active') ? panel.scrollHeight + "px" : null;
+});
 
-  btnAccordion.addEventListener('click', function() {
-    this.classList.toggle('active');
-    const panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-      this.classList.remove('active');
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-      this.classList.remove('active');
+// File Selection
+const fileSelection = (element, buttonText, fileType, extensions) => async () => {
+  try {
+    const selected = await window.__TAURI__.dialog.open({
+      filters: [{ name: fileType, extensions }],
+      directory: fileType === 'Directory',
+      multiple: false,
+    });
+    if (selected) {
+      element.value = selected;
+      buttonText.innerHTML = `File Selected <img src="assets/icons/check-svgrepo-com.svg" alt="File selected" width="24" height="24">`;
     }
-  });
+  } catch (err) {
+    // TODO: Error auf Display ausgeben
+    console.error(err);
+  }
+};
+
+// Assign Event Listeners
+btns.videoPath.addEventListener('click', fileSelection(elements.videoPath, btns.videoPath, 'Video Files', ['mp4']));
+btns.nmeaPath.addEventListener('click', fileSelection(elements.nmeaPath, btns.nmeaPath, 'NMEA Files', ['nmea']));
+btns.savePath.addEventListener('click', fileSelection(elements.savePath, btns.savePath, 'Directory'));
+btns.bitmask.addEventListener('click', fileSelection(elements.bitmaskPath, btns.bitmask, 'Bitmask file', ['txt']));
+
+
+// Start Script
+elements.startBtn.addEventListener('click', () => {
+
+  if (!areInputsValid()) {
+    // TODO: Alert einfügen und BTN noch ausblenden
+    console.log("Tryed to press STart")
+    return;
+  }
+
+  const startFrame = parseInt(elements.firstFrame.value, 10) || 0;
+  const startNMEALine = parseInt(elements.firstNMEALine.value, 10) || 0;
+  const bitmaskProvided = isNaN(Number(elements.bitmaskPath.value));
+  const cleanUp = elements.cleanup.checked;
+
+  const parameters = [
+    elements.videoPath.value,
+    elements.nmeaPath.value,
+    elements.bitmaskPath.value,
+    startFrame,
+    startNMEALine,
+    elements.savePath.value,
+    bitmaskProvided,
+    cleanUp
+  ];
+
+  const paramsObject = parameters.reduce((obj, param, index) => {
+    obj[`param${index + 1}`] = String(param);
+    return obj;
+  }, {});
+
+  window.__TAURI__.invoke('run_backend', paramsObject);
 });
